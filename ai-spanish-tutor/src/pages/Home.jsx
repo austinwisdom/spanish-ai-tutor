f8c087973246f66ae2003255d79124acf1b89832
austinwisdom <austin.n.wisdom@gmail.com>
2024-02-15 17:35:43 -0300

    Finished styling home.

--- ai-spanish-tutor/src/pages/Home.jsx
+++ ai-spanish-tutor/src/pages/Home.jsx
@@ -19,12 +19,30 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <h2 className=''>Save money and have fun while learning!</h2>
+            <div className='h-fit bg-white'>
+                <div className='flex flex-row justify-evenly'>
+                    <div className='self-center mb-2 drop-shadow-md'>
+                        <h2 className='text-2xl'>15 minutes a day is all it takes!</h2>
+                        <p>As with any skill, language learning requires frequent practice and commitment.</p>
+                        <p>Create a daily study routine to learn more effectively.</p>
+                    </div>
+                    <img className='h-96' src={text}/>
                 </div>
-                <div>
-
+                <div className='h-full flex flex-row justify-evenly'>
+                    <img className='h-96' src={money}/>
+                    <div className='self-center w-1/2 bg-blue-600 text-white rounded-full px-24 py-8'>
+                        <h2 className='text-2xl mb-2 drop-shadow-md'>Save money while learning!</h2>
+                        <p>You need hundreds of hours of speaking practice to attain fluency</p>
+                        <p>Hiring a human tutor by the hour really adds up!</p>
+                    </div>
+                </div>
+                <div className='flex flex-row justify-evenly'>
+                    <div className='self-center w-1/2 bg-yellow-300 rounded-full px-24 py-8'>
+                        <h2 className='text-2xl mb-2 drop-shadow-md'>Overcome nervousness while speaking!</h2>
+                        <p>It's natural to struggle with feeling embarrassed while speaking with native speakers, especially in the beginning of your journey.</p>
+                        <p>Get through the nerves by speaking first and frequently with Maxi, your friendly AI tutor!</p>
+                    </div>
+                    <img className='h-96' src={typing}/>
                 </div>
             </div>
             
