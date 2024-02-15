8ba9be686cddf5a34ed1581e4b3b0e6bec45e30f
austinwisdom <austin.n.wisdom@gmail.com>
2024-02-15 17:49:17 -0300

    Removed extra gifs.

--- ai-spanish-tutor/src/assets/gifs/homepage/index.js
+++ ai-spanish-tutor/src/assets/gifs/homepage/index.js
@@ -1,18 +1,9 @@
-import cat from './cat.gif'
-import eat from './eat.gif'
-import fox from './fox.gif'
-import hello from './hello.gif'
 import money from './money.gif'
-import run from './run.gif'
-import ski from './ski.gif'
-import spaceman from './spaceman.gif'
 import study from './study.gif'
-import surf from './surf.gif'
 import text from './text.gif'
 import typing from './type.gif'
-import sun from './sun.gif'
 import sunBlue from './sunBlue.gif'
 
 export {
-    cat, eat, fox, hello, money, run, ski, spaceman, study, surf, text, typing, sun, sunBlue
+    money, study, text, typing, sunBlue
 }
